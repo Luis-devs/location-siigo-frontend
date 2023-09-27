b3125178189fef947888dd4b469fb7070afc2abe
Luis-devs <luismonteria2001@gmail.com>
2023-09-27 14:06:34 -0500

    Previa de los puntos de la mejor ruta e inicialización del formulario

---
+++ src/components/Maps.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="plano">
+        <v-card>
+            <v-card-text class="d-flex">
+                <canvas id="pintar" ref="pintar" class="elevation-3 mx-auto" width="700" height="500"></canvas>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    name: "PlanoVue",
+    data: () => ({
+
+    }),
+    mounted() {
+        const lienzo = this.$refs.pintar.getContext('2d');
+        lienzo.lineWidth = 0.8; //Defino el ancho de la linea en pixeles
+        lienzo.strokeStyle = '#000000'; //Defino el color en hexagesimal
+
+        //EJEX
+        lienzo.beginPath(); // Pongo el lápiz
+        lienzo.moveTo(10, 0); // lo ubicó para iniciar el dibujo
+        lienzo.lineTo(10, 500); // trazo la linea hasta este punto
+        lienzo.stroke(); // levanto el lápiz
+        lienzo.closePath(); // me alisto para realizar otra parte del dibujo
+        //EJE Y
+        lienzo.beginPath(); // Pongo el lápiz
+        lienzo.moveTo(0, 490);// lo ubicó para iniciar el dibujo
+        lienzo.lineTo(700, 490);// trazo la linea hasta este punto
+        lienzo.stroke();// levanto el lápiz
+        lienzo.closePath();
+        const centerX = 100;
+        const centerY = 100;
+        const radius = 5;
+        lienzo.beginPath();
+        lienzo.fillStyle = '#0077aa';
+        lienzo.strokeStyle = '#0077aa47';
+        lienzo.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        lienzo.fill();
+        lienzo.stroke();
+        lienzo.closePath();
+
+        lienzo.beginPath();
+        lienzo.fillStyle = '#0077aa';
+        lienzo.strokeStyle = '#0077aa47';
+        lienzo.arc(200, 300, radius, 0, 2 * Math.PI, false);
+        lienzo.fill();
+        lienzo.stroke();
+        lienzo.closePath();
+        
+        lienzo.beginPath(); // Pongo el lápiz
+        lienzo.moveTo(100, 100);// lo ubicó para iniciar el dibujo
+        lienzo.lineTo(200, 300);// trazo la linea hasta este punto
+        lienzo.stroke();// levanto el lápiz
+        lienzo.closePath();
+    }
+}
+
+</script>
--- src/views/Buscar.vue
+++ src/views/Buscar.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-        <p>Buscar</p>
+    <div id="buscar">
+        <v-row justify="center" no-gutters>
+            <v-card width="600" max-with="700" class="my-auto mb-4">
+                <v-card-title justify="center">
+                    <h2>Mejor ruta</h2>
+                </v-card-title>
+                <v-card-text>
+                    <v-form
+                        ref="form"
+                        v-model="valid"
+                        lazy-validation>
+                        <v-autocomplete v-model="location" :items="locations" prepend-icon="mdi mdi-map-search" item-text="name" item-value="_id" no-data-text="Sin lugares para buscar rutas" :rules="autoRules" placeholder="Escoja un lugar"></v-autocomplete>
+                    </v-form>
+                </v-card-text>
+                <v-card-actions justify="center" class="flex-column">
+                    <v-btn
+                        color="success"
+                        class="mr-4"
+                        @click="buscarRuta">
+                        Buscar mejor ruta
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-row>
+        <Plano />
     </div>
 </template>
 <script>
+import Plano from "@/components/Maps"
 export default {
     name: "BuscarVue",
+    components: {
+        Plano
+    },
     data: () => ({
+        valid: true,
         url: `${process.env.VUE_APP_API_URL}:${process.env.VUE_APP_API_PORT}`,
+        locations: [],
+        location: null
     }), methods: {
         async checkSesion() {
             if (sessionStorage.currentSession) {
@@ -19,9 +49,13 @@ export default {
             } else {
                 this.$router.push('/');
             }
+        },
+        buscarRuta() {
+            console.log('ruta');
         }
     }, async created() {
         this.checkSesion();
+        await this.axios.get(`${this.url}/location`).then(response => this.locations = response.data);
     }
 }
 </script>
